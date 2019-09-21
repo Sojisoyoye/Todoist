f35f98655f49da0092d5731d074f0479232c8877
Sojisoyoye <soji.soyoye@gmail.com>
2019-09-21 23:24:52 +0100

    Testing app on develop

--- app/src/App.js
+++ app/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const App = () =>  (
     <div className="App">
       <header className="App-header">
-       <p>Hello Todoist!!!</p>
+       <p>Hello All, Welcome to my New App</p>
       </header>
     </div>
 );
