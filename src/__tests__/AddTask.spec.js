e1eae4c710070160262930c3bb122a0db41d4527
Sojisoyoye <soji.soyoye@gmail.com>
2019-10-03 11:26:22 +0100

    work in progress

---
+++ src/__tests__/AddTask.spec.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+// import React from 'react';
+// import { render, cleanup } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import { AddTask } from '../components/AddTask';
+
+// beforeEach(cleanup());
+
+// describe('<AddTask />', () => {
+//     it()
+// })
